5fc574116c6212e3977cd61fd9a84f66f75a66cc
Ben.Harrison <Ben.Harrison@Snowdengroup.com>
2017-08-15 17:15:35 +0800

    WREC4-12 - Modified the MockImport service to get it working and used it to test bringing in SampleCount data from MQ2

--- MockImports/Database/Schema/Procedures/dbo.GetReconciliationMovements.sql
+++ MockImports/Database/Schema/Procedures/dbo.GetReconciliationMovements.sql
@@ -1,15 +1,3 @@
-USE [ReconcilorImportMockWS]
-GO
-
-/****** Object:  StoredProcedure [dbo].[GetReconciliationMovements]    Script Date: 07/11/2013 11:34:44 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetReconciliationMovements]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [dbo].[GetReconciliationMovements]
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 -- =============================================
 -- Author:		John Nickerson
 -- Create date: 2013-07-11
@@ -44,4 +32,7 @@ BEGIN
 	Where BlastedDate Between @iStartDate And @iEndDate
 	Or BlockedDate Between @iStartDate And @iEndDate
 END
+GO
+
+GRANT EXECUTE ON dbo.GetReconciliationMovements TO public
 GO
