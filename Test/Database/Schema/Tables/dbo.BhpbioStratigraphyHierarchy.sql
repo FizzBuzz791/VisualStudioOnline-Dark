cfd7d0572c404428122e4dd06e8ee782da0d5eef
Ben.Harrison <Ben.Harrison@Snowdengroup.com>
2017-08-18 14:57:28 +0800

    WREC4-36 - Initial database structure for Strat Hierarchy

---
+++ Test/Database/Schema/Tables/dbo.BhpbioStratigraphyHierarchy.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [dbo].[BhpbioStratigraphyHierarchy]
+(
+	[Id] INT NOT NULL IDENTITY, 
+	[Parentid] INT NULL,
+	[Stratigraphy] varchar(50) NOT NULL,
+    [StratigraphyHierarchyTypeId] INT NOT NULL, 
+    [Description] NVARCHAR(255) NOT NULL, 
+    [StratNum] VARCHAR(4) NOT NULL, 
+    [Colour] VARCHAR(25) NOT NULL,
+	[SortOrder] INT NOT NULL, 
+    CONSTRAINT [PK_BhpbioStratigraphyHierarchy] PRIMARY KEY CLUSTERED
+	(
+		[Id] ASC
+	),
+	CONSTRAINT FK_BhpbioStratigraphyHierarchy FOREIGN KEY ([ParentId])
+		REFERENCES [dbo].[BhpbioStratigraphyHierarchy] ([Id]),
+	CONSTRAINT FK_BhpbioStratigraphyHierarchyType FOREIGN KEY ([StratigraphyHierarchyTypeId])
+		REFERENCES [dbo].[BhpbioStratigraphyHierarchyType] ([Id])
+)
+
+GO
+CREATE NONCLUSTERED INDEX idx_BhpbioStratigraphyHierarchyParent
+ON dbo.[BhpbioStratigraphyHierarchy]([ParentId])
+GO
+CREATE UNIQUE NONCLUSTERED INDEX idx_BhpbioStratigraphyHierarchyStratNum
+ON dbo.[BhpbioStratigraphyHierarchy]([StratNum])
+GO
+
+
---
+++ Test/Database/Upgrade/4.1/4.1.0.0/_0002_AddStratigraphyHierarchyTablesAndPermissions.sql
@@ -0,0 +1,56 @@
+﻿IF OBJECT_ID('dbo.BhpbioStratigraphyHierarchy') IS NOT NULL 
+     DROP TABLE dbo.BhpbioStratigraphyHierarchy
+GO 
+
+IF OBJECT_ID('dbo.BhpbioStratigraphyHierarchyType') IS NOT NULL 
+     DROP TABLE dbo.BhpbioStratigraphyHierarchyType
+GO 
+
+
+CREATE TABLE [dbo].[BhpbioStratigraphyHierarchyType]
+(
+	[Id] INT NOT NULL IDENTITY, 
+    [Type] VARCHAR(100) NOT NULL, 
+    [Level] INT NOT NULL,
+	CONSTRAINT [PK_BhpbioStratigraphyHierarchyType] PRIMARY KEY CLUSTERED
+	(
+		[Id] ASC
+	)
+)
+GO
+CREATE UNIQUE NONCLUSTERED INDEX idx_BhpbioStratigraphyHierarchyType
+ON [dbo].[BhpbioStratigraphyHierarchyType]([Level])
+GO
+
+INSERT INTO [dbo].[BhpbioStratigraphyHierarchyType] ([Type], [Level]) VALUES ('Formation', 1)
+INSERT INTO [dbo].[BhpbioStratigraphyHierarchyType] ([Type], [Level]) VALUES ('Member', 2)
+INSERT INTO [dbo].[BhpbioStratigraphyHierarchyType] ([Type], [Level]) VALUES ('Strat Unit', 3)
+
+CREATE TABLE [dbo].[BhpbioStratigraphyHierarchy]
+(
+	[Id] INT NOT NULL IDENTITY, 
+	[Parentid] INT NULL,
+	[Stratigraphy] varchar(50) NOT NULL,
+    [StratigraphyHierarchyTypeId] INT NOT NULL, 
+    [Description] NVARCHAR(255) NOT NULL, 
+    [StratNum] VARCHAR(4) NOT NULL, 
+    [Colour] VARCHAR(25) NOT NULL,
+	[SortOrder] INT NOT NULL, 
+    CONSTRAINT [PK_BhpbioStratigraphyHierarchy] PRIMARY KEY CLUSTERED
+	(
+		[Id] ASC
+	),
+	CONSTRAINT FK_BhpbioStratigraphyHierarchy FOREIGN KEY ([ParentId])
+		REFERENCES [dbo].[BhpbioStratigraphyHierarchy] ([Id]),
+	CONSTRAINT FK_BhpbioStratigraphyHierarchyType FOREIGN KEY ([StratigraphyHierarchyTypeId])
+		REFERENCES [dbo].[BhpbioStratigraphyHierarchyType] ([Id])
+)
+
+GO
+CREATE NONCLUSTERED INDEX idx_BhpbioStratigraphyHierarchyParent
+ON dbo.[BhpbioStratigraphyHierarchy]([ParentId])
+GO
+CREATE UNIQUE NONCLUSTERED INDEX idx_BhpbioStratigraphyHierarchyStratNum
+ON dbo.[BhpbioStratigraphyHierarchy]([StratNum])
+GO
+
