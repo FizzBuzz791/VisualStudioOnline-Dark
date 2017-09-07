d18f61efecec99110a7a8aae84c7b5af22fbfa64
Trent Jones <trentwilliamjones@gmail.com>
2017-09-07 13:16:21 +0800

    Renamed test class

--- Test/Snowden.Reconcilor.Bhpbio.Report.UnitTests/Extensions/GenericDataTableExtensionsTests.cs
+++ Test/Snowden.Reconcilor.Bhpbio.Report.UnitTests/Extensions/GenericDataTableExtensionsTests.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 namespace Snowden.Reconcilor.Bhpbio.Report.UnitTests.Extensions
 {
     [TestFixture]
-    public class GenericDataTableExtensions
+    public class GenericDataTableExtensionsTests
     {
         private DataRow _sut;
 
