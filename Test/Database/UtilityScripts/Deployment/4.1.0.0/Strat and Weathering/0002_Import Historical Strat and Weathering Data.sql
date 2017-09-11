c78f7b69265a098f214acfe740b7a714386d88ac
Ben.Harrison <Ben.Harrison@Snowdengroup.com>
2017-09-11 17:27:04 +0800

    WREC4-43 - Strat and weathering import - first import records tested

--- Test/Database/UtilityScripts/Deployment/4.1.0.0/Strat and Weathering/0002_Import Historical Strat and Weathering Data.sql
+++ Test/Database/UtilityScripts/Deployment/4.1.0.0/Strat and Weathering/0002_Import Historical Strat and Weathering Data.sql
@@ -1,4 +1,16 @@
-﻿declare @value_in_use sql_variant
+﻿-- this directory must contain the csv, and the xml schema definition
+-- 'Resource Classification Migration.xml', the name of the csv doesn't
+-- matter, but make sure that its the only csv in the directory
+DECLARE @directory varchar(256) = 'C:\temp\'
+
+-- Load data from CSV files into [Staging].[StratNumImport] table
+DECLARE @sql varchar(max)
+Declare @file varchar(256)
+declare @newfileName varchar(256)
+declare @value_in_use sql_variant
+DECLARE @Block_ImportSyncTableId Int
+DECLARE @sourceRow XML
+DECLARE @importSyncRowId BIGINT
 
 select	@value_in_use = value_in_use
 from	sys.configurations
@@ -15,15 +27,7 @@ BEGIN
 	EXEC ('RECONFIGURE;')
 END
 
--- this directory must contain the csv, and the xml schema definition
--- 'Resource Classification Migration.xml', the name of the csv doesn't
--- matter, but make sure that its the only csv in the directory
-DECLARE @directory varchar(256) = 'C:\temp\'
 
--- Load data from CSV files into [Staging].[ResourceClassificationTemp] table
-DECLARE @sql varchar(max)
-Declare @file varchar(256)
-declare @newfileName varchar(256)
 
 DECLARE @fileList TABLE
 (
@@ -39,20 +43,21 @@ END
 
 CREATE TABLE #StratWeatheringImportFile 
 (
-	SITE varchar(31) NULL ,					--1
-	PIT varchar(31) NULL,					--2
-	BENCH varchar(31) NULL,					--3
-	PATTERN varchar(31) NULL,				--4
-	BLOCK_NAME varchar(50) NULL,			--5
-	STRATIGRAPHY varchar(50) NULL,			--6
-	GEOMET_STRATNUM varchar(7) NULL,		--7
+	SITE varchar(31) COLLATE Latin1_General_CI_AS NULL ,					--1
+	PIT varchar(31) COLLATE Latin1_General_CI_AS NULL,					--2
+	BENCH varchar(31) COLLATE Latin1_General_CI_AS NULL,					--3
+	PATTERN varchar(31) COLLATE Latin1_General_CI_AS NULL,				--4
+	BLOCK_NAME varchar(50) COLLATE Latin1_General_CI_AS NULL,			--5
+	STRATIGRAPHY varchar(50) COLLATE Latin1_General_CI_AS NULL,			--6
+	GEOMET_STRATNUM varchar(7) COLLATE Latin1_General_CI_AS NULL,		--7
 	GEOMET_WEATHERING INT NULL,				--8
 	BLOCK_GUID varchar(32) NULL,			--9
-	[GUID] varchar(36) NULL,				--NA
+	[GUID] varchar(36) COLLATE Latin1_General_CI_AS NULL,				--NA
 	[DIGBLOCK_ID] varchar(36) COLLATE Latin1_General_CI_AS NULL, 	--NA
 	[DIGBLOCK_FOUND] BIT NOT NULL DEFAULT 0,
 	[STRATNUM_FOUND] BIT NOT NULL DEFAULT 0,
-	[WEATHERING_FOUND] BIT NOT NULL DEFAULT 0
+	[WEATHERING_FOUND] BIT NOT NULL DEFAULT 0,
+	[IMPORT_SYNC_ROW_ID] BIGINT NULL
 )
 
 -- Get List of csv files in a folder
@@ -120,6 +125,35 @@ UPDATE	#StratWeatheringImportFile
 UPDATE	#StratWeatheringImportFile
 	SET WEATHERING_FOUND = CASE WHEN EXISTS(SELECT 1 FROM dbo.BhpbioWeathering WHERE DisplayValue = #StratWeatheringImportFile.GEOMET_WEATHERING) THEN 1 ELSE 0 END
 
+
+-- Determine the Table Ids
+SELECT	@Block_ImportSyncTableId = ImportSyncTableId
+FROM	ImportSyncTable
+WHERE	Name = 'BlastModelBlockWithPointAndGrade'
+
+
+DELETE FROM Staging.Tmp_BlockModelSyncRow
+		
+-- get the Block data for all sync rows
+INSERT INTO Staging.Tmp_BlockModelSyncRow (ImportSyncRowId, Pit, Bench, Pattern, BlockName)
+SELECT	isr.ImportSyncRowId, 
+		isr.SourceRow.value('(/BlockModelSource/BlastModelBlockWithPointAndGrade/Pit)[1]', 'nvarchar(10)') as Pit,
+		isr.SourceRow.value('(/BlockModelSource/BlastModelBlockWithPointAndGrade/Bench)[1]', 'nvarchar(4)') as Bench,
+		isr.SourceRow.value('(/BlockModelSource/BlastModelBlockWithPointAndGrade/PatternNumber)[1]', 'nvarchar(4)') as PatternNumber,
+		isr.SourceRow.value('(/BlockModelSource/BlastModelBlockWithPointAndGrade/BlockName)[1]', 'nvarchar(14)') as BlockName
+FROM	ImportSyncQueue isq INNER JOIN ImportSyncRow isr ON isr.ImportSyncRowId = isq.ImportSyncRowId
+WHERE	isq.ImportId = 1
+AND		isq.SyncAction IN ('I','U')
+AND		isr.IsCurrent = 1
+AND		isq.IsPending = 0
+AND		isr.ImportSyncTableId = @Block_ImportSyncTableId
+AND		isr.SourceRow.exist('(/BlockModelSource/BlastModelBlockWithPointAndGrade/ModelName[.="Grade Control"])[1]') = 1
+ORDER BY isq.ImportSyncRowId
+
+
+UPDATE Staging.Tmp_BlockModelSyncRow
+	SET [DIGBLOCK_ID] = Pit + '-' + Right('0000' + Bench, 4) + '-' + Right('0000' + Pattern, 4) + '-' + BlockName
+
 SELECT	*
 FROM	#StratWeatheringImportFile
 
@@ -132,8 +166,6 @@ FROM		#StratWeatheringImportFile
 WHERE		NOT EXISTS (select	1 
 						FROM	[Staging].[Tmp_StratWeatheringImport]
 						WHERE	[Staging].[Tmp_StratWeatheringImport].[DIGBLOCK_ID] = #StratWeatheringImportFile.[DIGBLOCK_ID])
-
-
 
 
 DROP TABLE #StratWeatheringImportFile
